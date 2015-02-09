2066a43b8223460ae2823cbc608aaa6563d91d13
Sam Wang <w.sam@givreardent.net>
2015-02-08 19:04:26 -0500

    Revised initial commit.

---
+++ uW CIV/src/net/givreardent/sam/uwciv/MainActivity.java
@@ -0,0 +1,14 @@
+package net.givreardent.sam.uwciv;
+
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
